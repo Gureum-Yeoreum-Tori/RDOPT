50d6e6e97183d790a8e5bd94c41704c79b7489cd
Gureum-and-Yeoreum <yeoreumcloud@gmail.com>
2025-08-13 03:34:08 +0000

    gpu test 코드 수정

--- HellWorld.py
+++ HellWorld.py
@@ -1,8 +1,11 @@
-print("Hell World!")
-print("We live in Hell!")
+import torch
+
 print("We live in Hell! we live in hell...")
 
-import torch
-print(torch.cuda.is_available())
-print(torch.cuda.get_device_name(0))
-print(torch.cuda.device_count())
+print("CUDA available:", torch.cuda.is_available())
+if torch.cuda.is_available():
+    print("GPU 이름:", torch.cuda.get_device_name(0))
+    x = torch.rand(2143, 2143).cuda()
+    print("Tensor on GPU:", x)
+else:
+    print("GPU를 사용할 수 없습니다.")
