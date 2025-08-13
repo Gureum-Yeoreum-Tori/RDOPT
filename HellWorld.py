12b9c3c2ee5fdfc571cf9014a3fcb1e299a09051
Gureum-and-Yeoreum <yeoreumcloud@gmail.com>
2025-08-13 03:05:10 +0000

    added torch gpu check

--- HellWorld.py
+++ HellWorld.py
@@ -1,3 +1,8 @@
 print("Hell World!")
 print("We live in Hell!")
 print("We live in Hell! we live in hell...")
+
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.get_device_name(0))
+print(torch.cuda.device_count())
